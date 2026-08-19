166942bd54c8908ed037d403ba0fe2f53d09dc11
srivigna-115 <srivignapalnati007@gmail.com>
2026-08-19 19:17:36 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -9,7 +9,7 @@ public class Factorial {
     }
 
     public static void main(String[] args) {
-        int number = 20;
+        int number = 40;
         System.out.println("Factorial of " + number + " is " + compute(number));
     }
 }
